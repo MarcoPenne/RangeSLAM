84caa5c7953c20be4496fb6ce8a320b0cc67966d
MarcoPenne <marco.pennese.97@gmail.com>
2021-01-18 12:24:51 +0100

    plot text IDs when drawing landmarks

--- Code/visualization/drawLandmarks.m
+++ Code/visualization/drawLandmarks.m
@@ -4,5 +4,6 @@ function point = drawLandmarks(landmarks, color)
     for i=1:size(landmarks, 2)
         hold on;
         point = plot(landmarks(i).x_pose, landmarks(i).y_pose, color, "linewidth", 2);
+        text(landmarks(i).x_pose, landmarks(i).y_pose, int2str(landmarks(i).id))
     end
 end
