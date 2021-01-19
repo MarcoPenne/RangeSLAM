a1fbecb1354f244b9716101c28cb4abdce7dee9e
MarcoPenne <marco.pennese.97@gmail.com>
2021-01-19 22:12:08 +0100

    correspondence lines are dashed and gray

--- Code/visualization/drawLandCorrespondences.m
+++ Code/visualization/drawLandCorrespondences.m
@@ -6,7 +6,7 @@ function [] = drawLandCorrespondences(l_gt, l_opt)
     for i=1:size(l_opt, 2)
         for j=1:size(l_gt, 2)
             if l_opt(i).id == l_gt(j).id
-                plot([l_opt(i).x_pose l_gt(i).x_pose], [l_opt(i).y_pose l_gt(i).y_pose], 'r')
+                plot([l_opt(i).x_pose l_gt(i).x_pose], [l_opt(i).y_pose l_gt(i).y_pose], 'Color','#AAAAAA', 'LineStyle', ':' )
             end
         end
     end
