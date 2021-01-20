26878fec08604bed4c12dbf2cb9acc2305e5b0c0
MarcoPenne <marco.pennese.97@gmail.com>
2021-01-20 15:11:53 +0100

    initialize landmarks with LS

---
+++ Code/utils/LS_triangulateCircles.m
@@ -0,0 +1,89 @@
+function point = LS_triangulateCircles(centers, radii)
+
+    % error_func = createErrorFunction(centers, radii);
+
+    % options = optimset('MaxIter', 1000);
+    [radius_max, index_max] = max(radii);
+    center_max = centers(index_max, :);
+    %point_max = fminsearch(error_func, center_max);
+    [radius_min, index_min] = min(radii);
+    center_min = centers(index_min, :);
+    % point_min = fminsearch(error_func, center_min);
+    % if error_func(point_min) <= error_func(point_max)
+    %     point = point_min;
+    % else
+    %     point = point_max;
+    % end
+
+    n_it=30;
+    X = center_min';
+    for i=1:n_it
+        H = zeros(2, 2);
+        b = zeros(2, 1);
+        chi_min = 0;
+        for i=1:size(centers,1)
+            xc = centers(i,1);
+            yc = centers(i,2);
+            C = [xc; yc];
+            r = radii(i);
+            if norm(X - C)>0
+                e = norm(X - C) - radii(i);
+                J = [(-xc+X(1))/norm(X - C),  (-yc+X(2))/norm(X - C)];
+
+                H = H + J'*J;
+                b = b + J'*e;
+                chi_min = chi_min + e'*e;
+            else
+                e = norm(X - C) - radii(i);
+                J = [1000,  1000];
+
+                H = H + J'*J;
+                b = b + J'*e;
+                chi_min = chi_min + e'*e;
+                
+            end
+        end
+        dx = -H\b;
+        dx = dx;
+        X = X+dx;
+    end
+    X_min = X;
+
+    X = center_max';
+    for i=1:n_it
+        H = zeros(2, 2);
+        b = zeros(2, 1);
+        chi_max = 0;
+        for i=1:size(centers,1)
+            xc = centers(i,1);
+            yc = centers(i,2);
+            C = [xc; yc];
+            r = radii(i);
+            if norm(X - C)>0
+                e = norm(X - C) - radii(i);
+                J = [(-xc+X(1))/norm(X - C),  (-yc+X(2))/norm(X - C)];
+
+                H = H + J'*J;
+                b = b + J'*e;
+                chi_max = chi_max + e'*e;
+            else
+                e = norm(X - C) - radii(i);
+                J = [1000,  1000];
+
+                H = H + J'*J;
+                b = b + J'*e;
+                chi_max = chi_max + e'*e;
+            end
+        end
+        dx = -H\b;
+        dx = 0.1*dx;
+        X = X+dx;
+    end
+    X_max = X;
+
+    %if chi_min < chi_max
+        point = X_min';
+    %else
+    %    point = X_max';
+    %end
+end
--- main.m
+++ main.m
@@ -17,36 +17,36 @@ addpath './Code/ICP'
 
 
 
-landmarks = initializeLandmarks(poses, observations);
-landmarks
-[poses_opt, landmarks_opt, chi_stats] = solveMultiICP(poses, landmarks, observations, transitions, 20);
-[poses_opt, landmarks_opt] = rotateAndTranslate(poses_opt, landmarks_opt)
+%landmarks = initializeLandmarks(poses, observations);
+landmarks_LS = LS_landmarks_initialization(poses, observations);
 
+[poses_opt, landmarks_opt, chi_stats] = solveMultiICP(poses, landmarks_LS, observations, transitions, 25);
+[poses, landmarks_LS] = rotateAndTranslate(poses, landmarks_LS);
+[poses_opt, landmarks_opt] = rotateAndTranslate(poses_opt, landmarks_opt);
+%poses_opt
 figure();
 plot(chi_stats)
 
 figure();
 fig = gcf ();
-set(fig, 'units', 'normalized', 'position', [0 0 0.75 1])
+set(fig, 'units', 'normalized', 'position', [0.125 0 0.75 1])
 p_gt = drawPoses(poses_gt, 'g*');
 p = drawPoses(poses, 'y*');
 
-%for i=1:size(landmarks_opt, 2)
-%    land_opt_ids(end+1) = landmarks_opt(i).id;
-%end
 %drawRanges(poses, observations, [7]);
 
 l_gt = drawLandmarks(landmarks_gt, 'b*');
-l = drawLandmarks(landmarks, 'c*');
+%l = drawLandmarks(landmarks, 'c*');
+l_LS = drawLandmarks(landmarks_LS, 'c*');
 
 p_opt = drawPoses(poses_opt, 'r*');
 l_opt = drawLandmarks(landmarks_opt, 'm*');
 
 drawLandCorrespondences(landmarks_gt, landmarks_opt, [0.66, 0.66, 0.66])
-drawLandCorrespondences(landmarks, landmarks_opt, [0.27, 0.27, 0.27])
+drawLandCorrespondences(landmarks_LS, landmarks_opt, [0.27, 0.27, 0.27])
+%drawLandCorrespondences(landmarks, landmarks_LS, [0.27, 0.27, 0.27])
 
-
-legend([p, p_opt, p_gt, l, l_opt, l_gt], {"Initial Poses", "Poses Optimizated", "Poses Ground truth", "Initial Landmarks", "Landmarks Optizated", "Landmarks ground truth"})
+legend([p, p_opt, p_gt, l_LS, l_opt, l_gt], {"Initial Poses", "Poses Optimizated", "Poses Ground truth", "Initial Landmarks", "Landmarks Optizated", "Landmarks ground truth"})
 
 
 %drawnow;
