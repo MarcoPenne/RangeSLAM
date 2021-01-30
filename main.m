599a8cea2ce65a654684833f6f35cb920d7092f4
MarcoPenne <marco.pennese.97@gmail.com>
2021-01-31 00:55:33 +0100

    updated main

--- main.m
+++ main.m
@@ -1,33 +1,38 @@
 close all
-clear
+clear all
 clc
 
-%addpath '../'
 addpath './Code/g2o_utils'
 addpath './Code/visualization'
 addpath './Code/utils'
 addpath './Code/ICP'
-%source "../tools/utilities/geometry_helpers_2d.m"
 
-%addpath "./solution" 
 
 %load ground truth dataset and initial guess dataset
 [landmarks_gt, poses_gt, transitions_gt, observations_gt] = loadG2o('./Dataset/slam2d_range_only_ground_truth.g2o');
 [~, poses, transitions, observations] = loadG2o('./Dataset/slam2d_range_only_initial_guess.g2o');
 
+% Get initial guess of landmarks using LS
+% The third parameter is a threshold, it discards two observations if they are too similar
+landmarks_LS = LS_landmarks_initialization(poses, observations, 0.5);
+printf("\nInitialized landmarks: %d\n\nStarting ICP...\n", size(landmarks_LS, 2))
 
+% Solve problem with LS
+[poses_opt, landmarks_opt, chi_stats] = solveMultiICP(poses, landmarks_LS, observations, transitions, 20);
 
-%landmarks = initializeLandmarks(poses, observations);
-landmarks_LS = LS_landmarks_initialization(poses, observations);
+% The solution is aligned with the first pose of the initial guess (prior added)
+% but we want a beautiful plot (FOR VISUAL PURPOSES [not mandatory])
+% This transforms to have the first pose in the give position
+[poses, landmarks_LS] = rotateAndTranslate(poses, landmarks_LS, [0; 0; 0]);
+[poses_opt, landmarks_opt] = rotateAndTranslate(poses_opt, landmarks_opt, [0; 0; 0]);
+[poses_gt, landmarks_gt] = rotateAndTranslate(poses_gt, landmarks_gt, [0; 0; 0]);
 
-[poses_opt, landmarks_opt, chi_stats] = solveMultiICP(poses, landmarks_LS, observations, transitions, 25);
-[poses, landmarks_LS] = rotateAndTranslate(poses, landmarks_LS);
-[poses_opt, landmarks_opt] = rotateAndTranslate(poses_opt, landmarks_opt);
-%poses_opt
+% plot error stats
 figure();
 plot(chi_stats)
 title('Error during iterations');
 
+% Plot map and trajectory
 figure();
 title('Map and trajectory');
 fig = gcf ();
@@ -35,10 +40,7 @@ set(fig, 'units', 'normalized', 'position', [0.125 0 0.75 1])
 p_gt = drawPoses(poses_gt, 'g*');
 p = drawPoses(poses, 'y*');
 
-%drawRanges(poses, observations, [7]);
-
 l_gt = drawLandmarks(landmarks_gt, 'b*');
-%l = drawLandmarks(landmarks, 'c*');
 l_LS = drawLandmarks(landmarks_LS, 'c*');
 
 p_opt = drawPoses(poses_opt, 'r*');
@@ -46,7 +48,6 @@ l_opt = drawLandmarks(landmarks_opt, 'm*');
 
 drawLandCorrespondences(landmarks_gt, landmarks_opt, [0.66, 0.66, 0.66])
 drawLandCorrespondences(landmarks_LS, landmarks_opt, [0.27, 0.27, 0.27])
-%drawLandCorrespondences(landmarks, landmarks_LS, [0.27, 0.27, 0.27])
 
 legend([p, p_opt, p_gt, l_LS, l_opt, l_gt], {"Initial Poses", "Poses Optimizated", "Poses Ground truth", "Initial Landmarks", "Landmarks Optizated", "Landmarks ground truth"})
 
