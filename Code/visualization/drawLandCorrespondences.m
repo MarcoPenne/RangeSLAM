31b8bef346035263f1bc4277c2e0894280673b2a
MarcoPenne <marco.pennese.97@gmail.com>
2021-01-30 23:11:54 +0100

    bug fixes

--- Code/visualization/drawLandCorrespondences.m
+++ Code/visualization/drawLandCorrespondences.m
@@ -4,8 +4,8 @@ function [] = drawLandCorrespondences(l_gt, l_opt, color)
     hold on
     for i=1:size(l_opt, 2)
         for j=1:size(l_gt, 2)
-            if l_opt(i).id == l_gt(j).id
-                plot([l_opt(i).x_pose l_gt(i).x_pose], [l_opt(i).y_pose l_gt(i).y_pose], 'Color', color, 'LineStyle', ':' )
+            if l_opt(i).id == l_gt(j).id    
+                plot([l_opt(i).x_pose l_gt(j).x_pose], [l_opt(i).y_pose l_gt(j).y_pose], 'Color', color, 'LineStyle', ':' )
             end
         end
     end
