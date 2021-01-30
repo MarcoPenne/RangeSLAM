88f3e9d3659367f2e7f47ca50dacebb865e80003
MarcoPenne <marco.pennese.97@gmail.com>
2021-01-30 23:13:16 +0100

    added distance matrix

---
+++ Code/utils/distanceMatrix.m
@@ -0,0 +1,13 @@
+function dm = distanceMatrix(centers)
+
+    dm = zeros(size(centers, 1), size(centers, 1));
+    for i=1:size(centers, 1)
+        for j=i:size(centers, 1)
+            d = norm(centers(i, :)' - centers(j, :)');
+            dm(i, j) = d;
+            dm(j, i) = d;
+
+        end
+    end
+
+end
